156fd69a63faa2ad3f9a10097e4669fc9240b53e
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2022-05-19 12:20:07 +0500

    Created.

---
+++ Lab_14_05_06/Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Lab_14_05_06
+{
+    internal sealed partial class Form1 : Form
+    {
+        internal Form1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
