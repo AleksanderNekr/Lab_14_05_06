be3b86c252a4df87de4908d0ab6a0458a41505d4
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2022-05-22 20:53:20 +0500

    Collection output added.

--- Lab_14_05_06/Form1.Designer.cs
+++ Lab_14_05_06/Form1.Designer.cs
@@ -29,11 +29,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components    = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize    = new System.Drawing.Size(800, 450);
+            this.menuStrip1                            = new System.Windows.Forms.MenuStrip();
+            this.CreateMainCollectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ListBox                               = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.CreateMainCollectionToolStripMenuItem });
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name     = "menuStrip1";
+            this.menuStrip1.Size     = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text     = "menuStrip1";
+            // 
+            // CreateMainCollectionToolStripMenuItem
+            // 
+            this.CreateMainCollectionToolStripMenuItem.Name  =  "CreateMainCollectionToolStripMenuItem";
+            this.CreateMainCollectionToolStripMenuItem.Size  =  new System.Drawing.Size(128, 20);
+            this.CreateMainCollectionToolStripMenuItem.Text  =  "Создать коллекцию";
+            this.CreateMainCollectionToolStripMenuItem.Click += new System.EventHandler(this.CreateMainCollectionToolStripMenuItem_Click);
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location          = new System.Drawing.Point(12, 27);
+            this.ListBox.Name              = "ListBox";
+            this.ListBox.Size              = new System.Drawing.Size(776, 407);
+            this.ListBox.TabIndex          = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode       = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize          = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name          = "Form1";
             this.Text          = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.ListBox ListBox;
+
+        private System.Windows.Forms.MenuStrip         menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem CreateMainCollectionToolStripMenuItem;
 
         #endregion
     }
--- Lab_14_05_06/Form1.cs
+++ Lab_14_05_06/Form1.cs
@@ -1,12 +1,58 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using WorkersLib;
 
 namespace Lab_14_05_06
 {
     internal sealed partial class Form1 : Form
     {
+        private       Queue<List<Person>> _factory       = new Queue<List<Person>>();
+        private const int                 WorkshopsCount = 10;
+        private const int                 PeopleCount    = 5;
+
         internal Form1()
         {
             this.InitializeComponent();
+        }
+
+        private void PrintFactoryToListBox()
+        {
+            this.ListBox.Items.Clear();
+            var workshopsCount = 0;
+            foreach (List<Person> workshop in this._factory)
+            {
+                this.ListBox.Items.Add("Цех №" + ++workshopsCount);
+                foreach (Person person in workshop)
+                {
+                    if (person is Worker worker)
+                    {
+                        worker.WorkshopNumber = workshopsCount;
+                        this.ListBox.Items.Add(worker);
+                        continue;
+                    }
+
+                    this.ListBox.Items.Add(person);
+                }
+            }
+        }
+
+        private void CreateMainCollectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (var workshopIndex = 0; workshopIndex < WorkshopsCount; workshopIndex++)
+            {
+                var persons = new List<Person>();
+                for (var personIndex = 0; personIndex < PeopleCount; personIndex++)
+                {
+                    persons.Add(Person.GetRandomPerson());
+                }
+
+                this._factory.Enqueue(persons);
+            }
+
+            this.PrintFactoryToListBox();
         }
     }
 }
